c465eee1ab8677b7a1abc8acb36c66a2ba7a7db5
Vitaliy Hreva <vhreva@vhreva>
2021-04-06 03:17:30 +0300

    added method to get info about weather

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
 
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude)
-      setLong(position.coords.longitude)
+    const fetchData = async () => {  
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
     });
 
-    console.log('Latitude is: ', lat)
-    console.log('Longitude is: ', long)
+    
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });
+    }
+    debugger
+    fetchData();
   }, [lat, long])
 
   return (
